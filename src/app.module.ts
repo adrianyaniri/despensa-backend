ab990c20e11a72c55efe353b46e83b261f452622
Adrian <agyaniri@gmail.com>
2023-03-09 18:44:48 -0300

    base de datos levantada y configurada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
